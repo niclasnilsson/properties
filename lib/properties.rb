c8c8d8116160c085a24c4c2f6f56162bee7e6cdb
Niclas Nilsson <niclas.nilsson@factor10.com>
2010-05-07 06:55:42 +0200

    First rudimentary implementation

---
+++ lib/properties.rb
@@ -0,0 +1,73 @@
+
+
+module Properties
+  class Property
+    attr_reader :name, :kind
+    
+    def initialize name, kind
+      @name = name
+      @kind = kind
+    end
+  end
+  
+  
+end
+
+module Properties
+  class Value
+    attr_reader :property
+    attr_accessor :value
+    
+    def initialize value, property
+      @value = value
+      @property = property
+    end
+  end
+end
+
+
+module Properties
+  class PropertyError < RuntimeError
+  end
+end
+
+def property name, kind
+  @__properties__ ||= {}
+  @__properties__[name] = Properties::Property.new(name, kind)
+  
+  def __properties__
+    @__properties__
+  end
+
+  
+  self.send :define_method, :__set_value__ do |name, value|
+    @__property_values__ ||= {}
+
+    property = self.class.__properties__[name]
+    
+    # Validate type
+    kind = case property.kind
+           when :string then String
+           when :integer then Integer
+           else
+             NilClass
+           end
+
+    raise Properties::PropertyError.new("Can't set value (#{value}) to property #{name} since it's not a #{kind}") if not value.kind_of?(kind)
+
+    
+    @__property_values__[name] = Properties::Value.new(value, self.class.__properties__[name])
+  end
+  
+  self.send :define_method, "#{name}=" do |value|
+    __set_value__(name, value)
+  end
+
+  self.send :define_method, "#{name}" do
+    @__property_values__ ||= {}
+    
+    return nil if @__property_values__[name].nil?
+    @__property_values__[name].value 
+  end
+  
+end
--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -1,7 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Properties" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+require 'rspec-cleanup'
+
+describe "a property" do
+  cleanup.after_spec
+  
+  it "should have a name and a type" do
+    class Computer
+      property :name, :string
+    end
+  end
+  
+  before do
+    class Person
+      property :name, :string
+    end
+
+    @person = Person.new
+  end
+
+  describe "should have a set accessor that" do
+    it "accepts values of the specified type" do
+      @person.name = "Niclas"
+    end
+
+    it "rejects values of other types" do
+      lambda { @person.name = 1 }.should raise_error(Properties::PropertyError)
+    end
+  end
+  
+  describe "should have a get accessor that" do
+    it "returns the value of the property" do
+      @person.name = "Niclas"
+      @person.name.should == "Niclas"
+    end
+
+    it "returns nil if property has not been set" do
+      Person.new.name.should == nil
+    end
   end
 end
+
+
