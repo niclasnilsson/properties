af33ca9664597a14dba7847eabc8f4af3885a18b
Niclas Nilsson <niclas@niclasnilsson.se>
2011-08-31 09:28:56 +0200

    Initial support for collections

--- lib/properties.rb
+++ lib/properties.rb
@@ -1,65 +1,91 @@
 require 'facets'
-
+ 
 module Properties
   class Property
-    attr_reader :name, :kind
-    
-    def initialize name, kind
+    attr_reader :name, :kind, :is_collection
+   
+    def initialize name, kind, is_collection
       @name = name
       @kind = kind
+      @is_collection = is_collection
     end
   end
-  
-  
 end
-
+ 
 module Properties
   class Value
     attr_reader :property
     attr_accessor :value
-    
+   
     def initialize value, property
       @value = value
       @property = property
     end
   end
 end
-
-
+ 
 module Properties
   class PropertyError < RuntimeError
   end
 end
-
-def property name, kind
+ 
+def property name, kind, is_collection = false
   @__properties__ ||= {}
-  @__properties__[name] = Properties::Property.new(name, kind)
-  
+  @__properties__[name] = Properties::Property.new(name, kind, is_collection)
+  @is_collection = is_collection
+ 
   def __properties__
-    @__properties__
+   @__properties__
   end
-
-  
+   
   self.send :define_method, :__set_value__ do |name, value|
     @__property_values__ ||= {}
-
+ 
     property = self.class.__properties__[name]
-    
-    c = eval property.kind.to_s.camelcase(true)
-    raise Properties::PropertyError.new("Can't set value (#{value}) to property #{name} since it's not a #{kind}") if not value.kind_of?(c)
-    
+ 
+    c = eval property.kind.to_s.camelcase
+
+    if property.is_collection 
+      value.all? { |v| v.kind_of?(c) } 
+    else
+      raise Properties::PropertyError.new("Can't set value (#{value}) to property #{name} since it's not a #{kind}") if not value.kind_of?(c)
+    end
+
     @__property_values__[name] = Properties::Value.new(value, self.class.__properties__[name])
   end
-  
-  self.send :define_method, "#{name}=" do |value|
-    __set_value__(name, value)
-  end
 
-  self.send :define_method, "#{name}" do
-    @__property_values__ ||= {}
-    
-    return nil if @__property_values__[name].nil?
-    @__property_values__[name].value 
+  if is_collection
+    # Create a getter
+    self.send :define_method, "#{name}" do
+      @__property_values__ ||= {}
+     
+      __set_value__(name, []) if @__property_values__[name].nil?
+      @__property_values__[name].value
+    end
+
+    # Create a setter
+    self.send :define_method, "#{name}=" do |value|
+      __set_value__(name, value)
+    end
+  else
+    # Create a getter
+    self.send :define_method, "#{name}" do
+      @__property_values__ ||= {}
+     
+      return nil if @__property_values__[name].nil?
+      @__property_values__[name].value
+    end
+
+    # Create a setter
+    self.send :define_method, "#{name}=" do |value|
+      __set_value__(name, value)
+    end
+   
   end
-  
 end
+ 
+def collection name, kind
+  property name, kind, true
+end
+
+
--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -28,6 +28,7 @@ describe "a property" do
       lambda { @person.name = 1 }.should raise_error(Properties::PropertyError)
     end
   end
+
   
   describe "should have a get accessor that" do
     it "returns the value of the property" do
@@ -38,7 +39,101 @@ describe "a property" do
     it "returns nil if property has not been set" do
       Person.new.name.should == nil
     end
+
   end
 end
 
+describe "a collection property" do
+  cleanup.after_spec
+
+  before do
+    class Computer
+      collection :audio_inputs, :string
+    end
+    @computer = Computer.new
+  end
+
+  it "should be empty by default" do
+    @computer = Computer.new.audio_inputs.should == []
+  end
+
+  it "accepts appending of values" do
+    @computer.audio_inputs << "line in"
+    @computer.audio_inputs << "built-in microphone"
+
+    @computer.audio_inputs.should == ["line in", "built-in microphone"]
+    @computer.audio_inputs.should == ["line in", "built-in microphone"]
+    @computer.audio_inputs()[0].should == "line in"
+    @computer.audio_inputs()[1].should == "built-in microphone"
+  end
+  
+  it "accepts replacing of entire collection" do
+    @computer.audio_inputs << "line in"
+    @computer.audio_inputs << "microphone"
+
+    # Overwrite the entire collection
+    @computer.audio_inputs = ["usb microphone #1", "usb microphone #2"]
+
+    @computer.audio_inputs.should == ["usb microphone #1", "usb microphone #2"]
+  end
+
+  it "convert replacing entire collection with nil to an empty collection" do
+    pending
+    @computer.audio_inputs << "line in"
+    @computer.audio_inputs.size == 1
+    @computer.audio_inputs = nil
+    @computer.audio_inputs.should == []
+  end
+  
+  it "should reject appending values of wrong type" do
+    pending
+    lambda { @computer.audio_inputs << 1 }.should raise_error(Properties::PropertyError)
+  end
+
+  it "should reject overwrite of entire collection if new collection contains elements of wrong type" do
+    pending
+    lambda { @computer.audio_inputs = ["line in", 23] }.should raise_error(Properties::PropertyError)
+  end
+
+end
+
+describe "nested complex types" do
+
+  cleanup.after_spec
+
+  before do
+    class Disk
+     property :name, :string
+     property :gigabytes, :integer
+    end
+
+
+    class Computer
+      property :name, :string
+      collection :disks, :Disk
+
+    end
+    @computer = Computer.new
+  end
+
+  it "should worok on collections" do
+    @computer.name = "My Computer"
+    @computer.disks.should == []
+    disk1 = Disk.new
+    disk1.name = "First disk"
+    disk1.gigabytes = 200
+
+    disk2 = Disk.new
+    disk2.name = "Second disk"
+    disk2.gigabytes = 300
+
+    @computer.disks = [ disk1, disk2 ]
+
+    @computer.name.should == "My Computer"
+    @computer.disks[0].name.should == "First disk"
+    @computer.disks[0].gigabytes.should == 200
+    @computer.disks[1].name.should == "Second disk"
+    @computer.disks[1].gigabytes.should == 300
+  end
+end
 
